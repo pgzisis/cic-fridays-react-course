f676d9b53923797dd6f4b08c3031144c5f9f00cd
pgzisis <panagiotis.zisis-cic.netherlands@ibm.com>
2020-11-18 15:57:10 +0100

    Fix Spelling

--- src/Intro.js
+++ src/Intro.js
@@ -1,4 +1,4 @@
-// In this component we would like to return a <p>My name is *Your name* and I am learning React!<p>
+// In this Component we would like to return a <p>My name is *Your name* and I am learning React!<p>
 // When you are done go to the App.js file to finish the exercise!
 
-// Don't forget to export your component!
+// Don't forget to export your Component!
