d03a881aec49ec5f16410f08fd5cd1c58d1651ee
Athanasios Tsalkitzis <Athanasios.tsalkitzis-cic.netherlands@ibm.com>
2020-11-18 16:50:25 +0100

    tiny improvement in exercise steps

--- src/Hello.js
+++ src/Hello.js
@@ -1,4 +1,4 @@
-// In this Component we would like to return a <h1>Hello World!<h1> When you are
+// In this Component we would like to return a <h1>Hello World!</h1> When you are
 // done, go to the Intro.js file to continue!
 
 // Don't forget to export it too! Otherwise it won't be available to the rest of
