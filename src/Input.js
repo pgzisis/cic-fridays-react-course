83feda228d0071b5f900816f7f14f93a590f929a
Athanasios Tsalkitzis <Athanasios.tsalkitzis-cic.netherlands@ibm.com>
2020-11-25 14:29:54 +0100

    pass event in onchange handler

--- src/Input.js
+++ src/Input.js
@@ -1,9 +1,13 @@
-// Here, we render an input. When it's value changes, we want to show an alert
+// Here, we render an input. When it's value changes, we want to show an alert containing the input text
 // on the screen.
 
+// It's important to know that some event handlers also have access to the html event object
+// Such an event handler is the onChange input handler which has access to
+// the event.currentTarget.value field which contains the input.
+
 const Input = () => {
-  const handleChange = () => {
-    alert("Input's value changed!");
+  const handleChange = (event) => {
+    alert(event.currentTarget.value);
   };
 
   // Add the onChange event handler on the input, which will call
