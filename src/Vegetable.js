a9920ccb83a62d22ad4ceb703f09f7532f98e078
pgzisis <panagiotis.zisis-cic.netherlands@ibm.com>
2020-12-02 19:57:32 +0100

    Edit comments

--- src/Vegetable.js
+++ src/Vegetable.js
@@ -1,7 +1,7 @@
 // Here in this component first we should pass the props object in the required parameters.
-// After doing so we just have to replace the *I want to display a veggie here* field of our props object.
+// After doing so, we just have to replace the *I want to display a veggie here* with the `name` prop.
 
-// **Hint If stuck you can look back on the props branch.
+// **Hint If stuck, you can look back on the props branch.
 
 const Vegetable = (props) => {
   return <p>I want to display a veggie here</p>;
